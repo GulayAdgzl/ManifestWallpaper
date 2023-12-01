8487f5c21fb9eae4bcdd387edcb10dbc2c746cf0
GulayAdgzl <44726684+GulayAdgzl@users.noreply.github.com>
2023-12-01 21:24:57 +0300

    update

---
+++ app/src/main/java/com/android/manifestwallpaper/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,4 @@
+package com.android.manifestwallpaper.adapter
+
+class ViewPagerAdapter {
+}
--- app/src/main/java/com/android/manifestwallpaper/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/android/manifestwallpaper/ui/fragment/MainFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.manifestwallpaper.R
 import com.android.manifestwallpaper.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
+    private val tabTitles= listOf("Home","Popular","Random","Categories")
+    private val fragments= listOf(HomeFragment(),PopularFragment(),RandomFragment(),CategoriesFragment())
     private lateinit var binding:FragmentMainBinding
 
 
@@ -18,7 +20,13 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding=FragmentMainBinding.inflate(layoutInflater,container,false)
+        initTabLayout()
         return binding.root
     }
 
+    private fun initTabLayout(){
+        TabLayoutMediator(binding.tabLayout,binding.viewPager){tab,position ->
+            tab.text=tabTitles[position]
+        }.attach()
+    }
 }
