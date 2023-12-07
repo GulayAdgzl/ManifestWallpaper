f6047afc94848e5af3a1c75c89543cd57b8c3477
Gülay ADIGÜZEL <44726684+GulayAdgzl@users.noreply.github.com>
2023-12-07 13:55:25 +0300

    Update Wallpaper.kt

--- app/src/main/java/com/android/manifestwallpaper/model/Wallpaper.kt
+++ app/src/main/java/com/android/manifestwallpaper/model/Wallpaper.kt
@@ -2,5 +2,6 @@ package com.android.manifestwallpaper.model
 
 data class Wallpaper (
     val id:String,
-    val urls:UnsplashPhotoUrls
+    val urls:UnsplashPhotoUrls,
+    val blurHash:String
     )
